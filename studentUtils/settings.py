6bfd3d62f4650e1309bf2000072ec73c2af987aa
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-10-01 15:36:12 -0400

    change case

--- studentUtils/settings.py
+++ studentUtils/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+_ol0*#1u59s%#rgr2hargw0s%3isn(b3w*v53tcv$35b_l7j&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-print(os.environ)
-DEBUG = "Render" not in os.environ
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.15", "9698-72-80-73-130.ngrok.io"]
 

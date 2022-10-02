1a3a03b0596036ff606c0fd26c55ee1ec7cb6daa
Nirjhor Nath <nirjhor.nath@gmail.com>
2022-10-01 20:53:03 -0400

    more debug disably things

--- studentUtils/settings.py
+++ studentUtils/settings.py
@@ -130,7 +130,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
